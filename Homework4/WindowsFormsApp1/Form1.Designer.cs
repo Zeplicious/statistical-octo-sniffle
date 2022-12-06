0ceabd4aa1cab5b65b252ea76f5cebdada050b0a
DanieleCorvari <104635833+Zeplicious@users.noreply.github.com>
2022-12-06 17:17:21 +0100

    Add files via upload

---
+++ Homework4/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(717, 413);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "P of success";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "N. tosses";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(328, 14);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(476, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "N. trials";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(533, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(685, 11);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(81, 20);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Absolute\r\n";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(773, 11);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(78, 20);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Relative";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(857, 11);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(97, 20);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Normalized";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(685, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(269, 413);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 467);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "F";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Homework4/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Graphics g;
+        public Graphics g2;
+        public Bitmap b;
+        public Bitmap b2;
+        Random rnd;
+        double ran;
+        Point xy1;
+        Point xy2;
+        double ratio;
+        double max_ratio;
+        LinkedList<double> results;
+        double p;
+        int tot_tosses;
+        int trials;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            xy1 = new Point();
+            xy2 = new Point();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine(tot_tosses);
+            SetUp();
+            int h = pictureBox1.Height-10;
+            max_ratio = 1;
+            if (sender == radioButton3)
+            {
+                max_ratio = Math.Sqrt(tot_tosses);
+            }
+
+            for (int j = 0; j < trials; j++)
+            {
+                
+                double current_avg = 0;
+                int current = 0;
+                for (int i = 0; i < tot_tosses; i++)
+                {
+
+                    xy1.X = RealX(i);
+                    xy1.Y = RealY(current_avg, h, max_ratio);
+                    if (sender == radioButton3) {
+                        ran = rnd.NextDouble();
+                        if (ran >p) current ++;
+                        current_avg = current / Math.Sqrt(i + 1);
+                    }
+                    else
+                    {
+                        current = 0;
+                        ran = rnd.NextDouble();
+                        if (ran <= p)
+                        {
+                            current = 1;
+                            
+                        }
+                        if (sender == radioButton1) current_avg += UpdateRatio(current, tot_tosses, current_avg, 0);
+                        else if (sender == radioButton2) current_avg += UpdateRatio(current, i, current_avg, 1);
+                        else if (sender == radioButton3) current_avg += UpdateRatio(current, tot_tosses, current_avg, 0) * Math.Sqrt(i + 1);
+
+                    }
+
+                    xy2.X = xy1.X;
+                    xy2.Y = RealY(current_avg, h, max_ratio);
+
+                    g.DrawLine(Pens.Black, xy1, xy2);
+
+                    xy1.Y = xy2.Y;
+                    xy2.X = RealX(i + 1);
+                    g.DrawLine(Pens.Black, xy1, xy2);
+
+                }
+                results.AddFirst(current_avg);
+
+            }
+            DrawGraph();
+        }
+
+ 
+
+        private void SetUp() 
+        {
+
+            p = Double.Parse(textBox1.Text);
+            tot_tosses = Int32.Parse(textBox2.Text);
+            trials = Int32.Parse(textBox3.Text);
+
+            ratio = (pictureBox1.Width-1) / tot_tosses;
+            //System.Diagnostics.Debug.WriteLine(tot_tosses);
+            b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(b);
+            pictureBox1.Image = b;
+            g.DrawRectangle(Pens.Black, (float)0, 0, pictureBox1.Width-1 , (float)pictureBox1.Height-1);
+            results = new LinkedList<double>();
+        }
+        private void DrawGraph()
+        {
+            b2 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            g2 = Graphics.FromImage(b2);
+            pictureBox2.Image = b2;
+            g.DrawRectangle(Pens.Black, (float)0, 0, pictureBox1.Width-1 , (float)pictureBox1.Height-1);
+            int[] array = new int[50];
+            for (int i = 0; i < array.Length; i++) { array[i] = 0; }
+            foreach (double result in results)
+            {
+                array[(int)Math.Round((1 - result/max_ratio) * 49)]++;
+            }
+            int max = 0;
+            foreach (int i in array)
+            {
+                if (i > max) max = i;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+
+
+                g2.DrawRectangle(Pens.Blue, (float)0, (float)(i) * (pictureBox2.Height-10) / 50, (float)array[i] * (pictureBox2.Width-5) / max, (float)(pictureBox2.Height-10) / 50);
+                g2.FillRectangle(Brushes.Blue, (float)0, (float)(i) * (pictureBox2.Height-10) / 50, (float)array[i] * (pictureBox2.Width-5) / max, (float)(pictureBox2.Height-10) / 50);
+            }
+            g2.DrawRectangle(Pens.Black, (float)0, 0, pictureBox2.Width-1, (float)pictureBox2.Height - 1);
+
+        }
+        private double UpdateRatio(int current, int toss,double ratio, int type) 
+        {
+            if (type == 0) return (double) (current)/toss;
+            else if(type==1) return (double) (current - ratio) / (toss + 1);
+            else return (double)((current - ratio)/Math.Sqrt(toss + 1));
+        }
+        private int RealX(int toss)
+        {
+            return (int)Math.Round((double)toss * ratio)+5;
+        }
+        private int RealY(double ratio,double h,double max_ratio)
+        {
+            return (int)Math.Round((1-ratio/max_ratio) * h)+5;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
